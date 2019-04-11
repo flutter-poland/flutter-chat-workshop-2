fe72196b15cdb768e25886d7f9cf206511c882d3
Dominik Roszkowski <kontakt@dominikroszkowski.pl>
2019-04-11 22:31:09 +0200

    Layout appbar of MessagesPage

--- lib/screens/messages_page.dart
+++ lib/screens/messages_page.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MessagesPage extends StatelessWidget {
+class MessagesPage extends StatefulWidget {
+  @override
+  _MessagesPageState createState() => _MessagesPageState();
+}
+
+class _MessagesPageState extends State<MessagesPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final subtitleStyle = TextStyle(fontSize: 10);
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Przykładowy pokój',
+              style: TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Online | 2 min ago',
+              style: subtitleStyle,
+            ),
+          ],
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.grey,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Icon(
+              FontAwesomeIcons.microphone,
+              color: Colors.grey,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
