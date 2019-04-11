dd64cd7b7d057e49b902a3c496a10b733d970bad
Dominik Roszkowski <kontakt@dominikroszkowski.pl>
2019-04-11 22:10:46 +0200

    Add layout of ChatRoomsPage

--- lib/screens/chat_rooms_page.dart
+++ lib/screens/chat_rooms_page.dart
@@ -1,8 +1,49 @@
+import 'package:chatapp/widgets/chat_room_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class ChatRoomsPage extends StatelessWidget {
+class ChatRoomsPage extends StatefulWidget {
+  @override
+  _ChatRoomsPageState createState() => _ChatRoomsPageState();
+}
+
+class _ChatRoomsPageState extends State<ChatRoomsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Center(
+          child: const Text('1 new message'),
+        ),
+        leading: const Icon(
+          Icons.search,
+          color: Colors.grey,
+        ),
+        actions: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Icon(
+              FontAwesomeIcons.edit,
+              size: 20,
+              color: Colors.grey,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        child: ListView.separated(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ChatRoomListTile(
+              index: index,
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          },
+        ),
+      ),
+    );
   }
 }
