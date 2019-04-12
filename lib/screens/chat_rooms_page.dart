426c1479dfd957fa17198e0502af8b4b7facfc96
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2019-04-12 14:49:06 +0200

    Connect chat rooms page with model

--- lib/screens/chat_rooms_page.dart
+++ lib/screens/chat_rooms_page.dart
@@ -2,6 +2,7 @@ import 'package:chatapp/models/chat_room_model.dart';
 import 'package:chatapp/widgets/chat_room_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class ChatRoomsPage extends StatefulWidget {
   @override
@@ -20,38 +21,45 @@ class _ChatRoomsPageState extends State<ChatRoomsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: Center(
-          child: const Text('1 new message'),
+    return ScopedModel(
+      model: _model,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Center(
+            child: const Text('1 new message'),
+          ),
+          leading: const Icon(
+            Icons.search,
+            color: Colors.grey,
+          ),
+          actions: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Icon(
+                FontAwesomeIcons.edit,
+                size: 20,
+                color: Colors.grey,
+              ),
+            )
+          ],
         ),
-        leading: const Icon(
-          Icons.search,
-          color: Colors.grey,
-        ),
-        actions: [
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Icon(
-              FontAwesomeIcons.edit,
-              size: 20,
-              color: Colors.grey,
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        child: ListView.separated(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return ChatRoomListTile(
-              index: index,
-            );
-          },
-          separatorBuilder: (context, index) {
-            return Divider();
-          },
+        body: Container(
+          child: ScopedModelDescendant<ChatRoomsModel>(
+            builder: (_, __, model) {
+              return ListView.separated(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return ChatRoomListTile(
+                    index: index,
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider();
+                },
+              );
+            },
+          ),
         ),
       ),
     );
